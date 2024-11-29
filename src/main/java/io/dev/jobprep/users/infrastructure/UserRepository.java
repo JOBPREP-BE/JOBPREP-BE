8dee7f3ebf17bda2677bda7c1aa990b766f453ec
서창덕 <44630705+Garodden@users.noreply.github.com>
2024-11-30 01:21:20 +0900

    [FEAT] user 정보 가져오는 메소드 추가

--- src/main/java/io/dev/jobprep/users/infrastructure/UserRepository.java
+++ src/main/java/io/dev/jobprep/users/infrastructure/UserRepository.java
@@ -3,6 +3,8 @@ package io.dev.jobprep.users.infrastructure;
 import io.dev.jobprep.users.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserById(Long id);
 }
