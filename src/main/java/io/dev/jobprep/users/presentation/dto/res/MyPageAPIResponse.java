f8b7a712f59f996540bc8415668c9958dc859952
서창덕 <44630705+Garodden@users.noreply.github.com>
2024-11-30 19:48:21 +0900

    FIX: MyPageAPIResponse 클래스가 직렬화 가능하도록 수정

--- src/main/java/io/dev/jobprep/users/presentation/dto/res/MyPageAPIResponse.java
+++ src/main/java/io/dev/jobprep/users/presentation/dto/res/MyPageAPIResponse.java
@@ -3,12 +3,15 @@ package io.dev.jobprep.users.presentation.dto.res;
 import io.dev.jobprep.users.application.dto.res.MyPageResponse;
 import io.dev.jobprep.users.domain.UserRole;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @AllArgsConstructor
 public class MyPageAPIResponse {
     private String userEmail;
     private String Username;
     private UserRole userRole;
+
 
     public static MyPageAPIResponse from(MyPageResponse myPageResponse){
         return new MyPageAPIResponse(myPageResponse.getUserEmail(),
