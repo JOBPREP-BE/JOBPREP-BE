774e4b12e7f4a6b35fa3c8532748581b998ba898
서창덕 <44630705+Garodden@users.noreply.github.com>
2024-12-01 20:18:02 +0900

    [FEAT] 회원가입 서비스로직 구현. 에러처리 로직 필요

--- src/main/java/io/dev/jobprep/users/application/UserService.java
+++ src/main/java/io/dev/jobprep/users/application/UserService.java
@@ -1,12 +1,15 @@
 package io.dev.jobprep.users.application;
+import io.dev.jobprep.users.application.dto.req.SignUpRequest;
 import io.dev.jobprep.users.application.dto.res.DeleteUserAccountResponse;
 import io.dev.jobprep.users.application.dto.res.MyPageResponse;
+import io.dev.jobprep.users.application.dto.res.SignUpResponse;
 import io.dev.jobprep.users.domain.User;
 import io.dev.jobprep.users.infrastructure.UserRepository;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDateTime;
@@ -19,6 +22,24 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+
+    @Transactional
+    public SignUpResponse SignUpUser(SignUpRequest signUpRequest) {
+        // 이메일 중복 확인
+        //TODO 탈퇴 유저 재가입시 로직 필요
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            throw new IllegalArgumentException("이미 사용 중인 이메일입니다.");
+        }
+
+        // 중복되지 않을 경우 새 사용자 저장
+        User newUser = User.builder()
+                .username(signUpRequest.getUsername())
+                .email(signUpRequest.getEmail())
+                .build();
+
+        //TODO error exception 처리
+        return SignUpResponse.from(userRepository.save(newUser));
+    }
     //TODO 나중에 JWT 관련 로직으로 처리하기
     public MyPageResponse getUserMyPageInfo(Long userId){
         User userData = userRepository.findUserById(userId)
