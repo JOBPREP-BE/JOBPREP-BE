b7a765d83e3fcc8b36668d05fc1bcc073cd179db
서창덕 <44630705+Garodden@users.noreply.github.com>
2024-12-01 21:51:18 +0900

    [FEAT] Email로 유저 찾도록 함수 추가

--- src/main/java/io/dev/jobprep/users/infrastructure/UserRepository.java
+++ src/main/java/io/dev/jobprep/users/infrastructure/UserRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserById(Long id);
-    Boolean existsByEmail(String email);
+    Optional<User> findUserByEmail(String email);
 }
