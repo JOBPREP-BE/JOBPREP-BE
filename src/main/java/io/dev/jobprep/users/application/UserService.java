83d55f35f161a31f57f8380ef10195f361a60354
서창덕 <44630705+Garodden@users.noreply.github.com>
2024-12-01 18:46:14 +0900

    [FEAT] 회원탈퇴 로직 메소드 deleteUserAcount 구현

--- src/main/java/io/dev/jobprep/users/application/UserService.java
+++ src/main/java/io/dev/jobprep/users/application/UserService.java
@@ -1,4 +1,5 @@
 package io.dev.jobprep.users.application;
+import io.dev.jobprep.users.application.dto.res.DeleteUserAccountResponse;
 import io.dev.jobprep.users.application.dto.res.MyPageResponse;
 import io.dev.jobprep.users.domain.User;
 import io.dev.jobprep.users.infrastructure.UserRepository;
@@ -8,6 +9,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
@@ -19,9 +21,8 @@ public class UserService {
 
     //TODO 나중에 JWT 관련 로직으로 처리하기
     public MyPageResponse getUserMyPageInfo(Long userId){
-        //TODO nullexception 처리
         User userData = userRepository.findUserById(userId)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));;
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
         return MyPageResponse.builder().userEmail(userData.getEmail())
                 .Username(userData.getUsername())
@@ -29,4 +30,17 @@ public class UserService {
                 .build();
 
     }
+
+    public DeleteUserAccountResponse deleteUserAcount(Long userId){
+        User userData = userRepository.findUserById(userId)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        LocalDateTime timeNow = userData.setDeletedAt();
+        userRepository.save(userData);
+
+        return DeleteUserAccountResponse.builder()
+                .time(timeNow)
+                .Message("회원탈퇴가 성공적으로 처리되었습니다.")
+                .build();
+    }
+
 }
