6c696830e32d8daf6e801d7eed977dfaaf85d82e
서창덕 <44630705+Garodden@users.noreply.github.com>
2024-12-01 18:46:24 +0900

    [FEAT] 회원탈퇴 API 구현

--- src/main/java/io/dev/jobprep/users/presentation/UserController.java
+++ src/main/java/io/dev/jobprep/users/presentation/UserController.java
@@ -2,16 +2,15 @@ package io.dev.jobprep.users.presentation;
 
 
 import io.dev.jobprep.users.application.UserService;
+import io.dev.jobprep.users.application.dto.res.DeleteUserAccountResponse;
 import io.dev.jobprep.users.application.dto.res.MyPageResponse;
+import io.dev.jobprep.users.presentation.dto.res.DeleteUserAccountAPIResponse;
 import io.dev.jobprep.users.presentation.dto.res.MyPageAPIResponse;
 import lombok.Getter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -27,4 +26,13 @@ public class UserController {
         MyPageResponse myPageResponse = userService.getUserMyPageInfo(userId);
         return ResponseEntity.ok(MyPageAPIResponse.from(myPageResponse));
     }
+
+    @DeleteMapping("/delete-account")
+    public ResponseEntity<DeleteUserAccountAPIResponse> deleteMyAccount(@RequestParam(required = false) Long userId/*,
+                                                    @AuthenticationPrincipal UserDetails userDetails*/){
+        DeleteUserAccountResponse response = userService.deleteUserAcount(userId);
+        return ResponseEntity.ok(DeleteUserAccountAPIResponse.from(response));
+
+    }
+
 }
