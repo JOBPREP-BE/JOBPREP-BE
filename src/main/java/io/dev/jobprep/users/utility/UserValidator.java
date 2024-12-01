9f9ecb4de8e9abb2d9bc7303075ad10944bd69c5
서창덕 <44630705+Garodden@users.noreply.github.com>
2024-12-01 21:50:16 +0900

    [FEAT] UUsreException 오타 수정

--- src/main/java/io/dev/jobprep/users/utility/UserValidator.java
+++ src/main/java/io/dev/jobprep/users/utility/UserValidator.java
@@ -1,13 +1,13 @@
 package io.dev.jobprep.users.utility;
 
 
-import io.dev.jobprep.exception.exception_class.UserEcxeption;
+import io.dev.jobprep.exception.exception_class.UserException;
 import io.dev.jobprep.users.domain.User;
 
 import static io.dev.jobprep.exception.code.ErrorCode400.USER_ACCOUNT_WITHDRAWN_ERROR;
 
 public class UserValidator {
     public static void validateUserActive(User user) {
-        if (user.getDeletedAt() != null) throw new UserEcxeption(USER_ACCOUNT_WITHDRAWN_ERROR);
+        if (user.getDeletedAt() != null) throw new UserException(USER_ACCOUNT_WITHDRAWN_ERROR);
     }
 }
