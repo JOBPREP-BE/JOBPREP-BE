df4a88858a11e49311101aaa7ddd34d5f78251e8
JIN-076 <andy230@naver.com>
2024-12-04 02:29:45 +0900

    [FEAT] ApplicationStatus 도메인 관련 Repository 구현

---
+++ src/main/java/io/dev/jobprep/domain/applicationstatus/infrastructure/ApplicationStatusJpaRepository.java
@@ -0,0 +1,10 @@
+package io.dev.jobprep.domain.applicationstatus.infrastructure;
+
+import io.dev.jobprep.domain.applicationstatus.domain.entity.ApplicationStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ApplicationStatusJpaRepository extends JpaRepository<ApplicationStatus, Long> {
+
+}
