96d7d87b5413c02546293942a6ea8e6469e175eb
Go-EunLee <koa789456@gmail.com>
2024-12-03 18:38:55 +0900

    [FIX] JobInterviewException /domain/job_interview/exception 폴더로 위치 이동

--- src/main/java/io/dev/jobprep/exception/exception_class/JobInterviewException.java
+++
@@ -1,9 +0,0 @@
-package io.dev.jobprep.exception.exception_class;
-
-import io.dev.jobprep.exception.code.ErrorCode;
-
-public class JobInterviewException extends CustomException{
-    public JobInterviewException(ErrorCode errorCode){
-        super(errorCode);
-    }
-}
