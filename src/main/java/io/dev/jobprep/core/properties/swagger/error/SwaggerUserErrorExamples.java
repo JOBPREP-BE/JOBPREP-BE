b89c73dd910cabc41a1ac2b6c9a3a83c5c7b35ea
서창덕 <44630705+Garodden@users.noreply.github.com>
2024-12-05 00:01:07 +0900

    [FEAT] 유저 스웨거 에러 정리

--- src/main/java/io/dev/jobprep/core/properties/swagger/error/SwaggerUserErrorExamples.java
+++ src/main/java/io/dev/jobprep/core/properties/swagger/error/SwaggerUserErrorExamples.java
@@ -3,4 +3,7 @@ package io.dev.jobprep.core.properties.swagger.error;
 public class SwaggerUserErrorExamples {
 
     public static final String USER_NOT_FOUND = "{\"timestamp\":\"2024-02-17T10:07:31.404Z\",\"statusCode\":404,\"code\":\"E03-USER-001\",\"message\":\"존재하지 않는 유저입니다.\"}";
+    public static final String USER_ACCOUNT_DISABLED = "{\"timestamp\":\"2024-02-17T10:07:31.404Z\",\"statusCode\":401,\"code\":\"E01-USER-001\",\"message\":\"탈퇴한 유저입니다\"}";
+    public static final String USER_ACCOUNT_ALREADY_EXISTS = "{\"timestamp\":\"2024-02-17T10:07:31.404Z\",\"statusCode\":400,\"code\":\"E01-USER-001\",\"message\":\"해당 이메일로 가입된 계정이 이미 존재합니다.\"}";
+
 }
