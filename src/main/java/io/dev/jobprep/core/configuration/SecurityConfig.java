f8a199c4afdcf36ff21e010f8a29cc3ac94bf3fe
Go-EunLee <koa789456@gmail.com>
2024-12-11 21:51:38 +0900

    [FEAT] OAuth security config 설정

--- src/main/java/io/dev/jobprep/core/configuration/SecurityConfig.java
+++ src/main/java/io/dev/jobprep/core/configuration/SecurityConfig.java
@@ -1,15 +1,46 @@
 package io.dev.jobprep.core.configuration;
 
+import io.dev.jobprep.security.oauth.CustomOAuth2UserService;
+import io.dev.jobprep.security.oauth.OAuth2SuccessHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
+@RequiredArgsConstructor
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+
+    private final CustomOAuth2UserService oAuth2UserService;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+
+                .authorizeHttpRequests(request ->
+                        request.anyRequest().permitAll()
+                )
+
+                .oauth2Login(oauth ->
+                        oauth.userInfoEndpoint(oa -> oa.userService(oAuth2UserService))
+                                .successHandler(oAuth2SuccessHandler)
+
+                );
+
+        return http.build();
+    }
 
     @Bean
     SecurityFilterChain filterChainApi(HttpSecurity httpSecurity) throws Exception {
