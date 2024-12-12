e0f2d53bec69e92257de13b84aa8ac856582d9f2
JIN-076 <andy230@naver.com>
2024-12-12 16:53:07 +0900

    [FEAT] Chat 도메인 API 관련 Controller 구현 및 Swagger 반영

---
+++ src/main/java/io/dev/jobprep/common/swagger/template/ChatSwagger.java
@@ -0,0 +1,77 @@
+package io.dev.jobprep.common.swagger.template;
+
+import io.dev.jobprep.core.properties.swagger.error.SwaggerChatErrorExamples;
+import io.dev.jobprep.core.properties.swagger.error.SwaggerUserErrorExamples;
+import io.dev.jobprep.domain.chat.presentation.dto.res.ChatMessageCommonResponse;
+import io.dev.jobprep.domain.chat.presentation.dto.res.ChatRoomAdminResponse;
+import io.dev.jobprep.exception.dto.ErrorResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public interface ChatSwagger {
+
+    @Operation(summary = "채팅 메시지 내역 조회", description = "사용자가 관리자와의 채팅 메시지 내역을 조회할 때 사용하는 API")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "채팅 메시지 내역 조회 성공"),
+        @ApiResponse(responseCode = "404", description = "요청한 데이터가 존재하지 않음",
+            content = @Content(
+                mediaType = "application/json",
+                schema = @Schema(implementation = ErrorResponse.class),
+                examples = @ExampleObject(name = "E03-USER-001", value = SwaggerUserErrorExamples.USER_NOT_FOUND)
+            ))
+    })
+    ResponseEntity<List<ChatMessageCommonResponse>> getMyMessageHistory(@RequestParam Long userId);
+
+    @Operation(summary = "채팅 메시지 내역 조회", description = "관리자가 유저와의 채팅 메시지 내역을 조회할 때 사용하는 API")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "채팅 메시지 내역 조회 성공"),
+        @ApiResponse(responseCode = "403", description = "메시지 내역을 조회할 권한이 없음",
+            content = @Content(
+                mediaType = "application/json",
+                schema = @Schema(implementation = ErrorResponse.class),
+                examples = @ExampleObject(name = "E02-CHATROOM-001", value = SwaggerChatErrorExamples.CHAT_ROOM_FORBIDDEN_OPERATION)
+            )),
+        @ApiResponse(responseCode = "404", description = "요청한 데이터가 존재하지 않음",
+            content = @Content(
+                mediaType = "application/json",
+                schema = @Schema(implementation = ErrorResponse.class),
+                examples = {
+                    @ExampleObject(name = "E03-USER-001", value = SwaggerUserErrorExamples.USER_NOT_FOUND),
+                    @ExampleObject(name = "E03-CHATROOM-001", value = SwaggerChatErrorExamples.CHATROOM_NOT_FOUND)
+                }
+            ))
+    })
+    ResponseEntity<List<ChatMessageCommonResponse>> getUsersMessageHistoryForAdmin(
+        @RequestParam Long userId,
+        @PathVariable String id
+    );
+
+    @Operation(summary = "활성화된 채팅방 리스트 조회", description = "관리자가 활성화된 채팅방 목록을 조회할 때 사용하는 API")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "채팅방 리스트 조회 성공"),
+        @ApiResponse(responseCode = "403", description = "메시지 내역을 조회할 권한이 없음",
+            content = @Content(
+                mediaType = "application/json",
+                schema = @Schema(implementation = ErrorResponse.class),
+                examples = @ExampleObject(name = "E02-CHATROOM-001", value = SwaggerChatErrorExamples.CHAT_ROOM_FORBIDDEN_OPERATION)
+            )),
+        @ApiResponse(responseCode = "404", description = "요청한 데이터가 존재하지 않음",
+            content = @Content(
+                mediaType = "application/json",
+                schema = @Schema(implementation = ErrorResponse.class),
+                examples = @ExampleObject(name = "E03-USER-001", value = SwaggerUserErrorExamples.USER_NOT_FOUND)
+            ))
+    })
+    ResponseEntity<List<ChatRoomAdminResponse>> getActiveChatRoomsForAdmin(
+        @RequestParam Long userId
+    );
+
+}
---
+++ src/main/java/io/dev/jobprep/domain/chat/presentation/ChatRoomController.java
@@ -0,0 +1,57 @@
+package io.dev.jobprep.domain.chat.presentation;
+
+import io.dev.jobprep.common.swagger.template.ChatSwagger;
+import io.dev.jobprep.domain.chat.application.ChatService;
+import io.dev.jobprep.domain.chat.presentation.dto.res.ChatMessageCommonResponse;
+import io.dev.jobprep.domain.chat.presentation.dto.res.ChatRoomAdminResponse;
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/chatrooms")
+@RequiredArgsConstructor
+public class ChatRoomController implements ChatSwagger {
+
+    private final ChatService chatService;
+
+    @GetMapping("/my")
+    public ResponseEntity<List<ChatMessageCommonResponse>> getMyMessageHistory(@RequestParam Long userId) {
+
+        return ResponseEntity.ok(chatService.getMessageHistory(userId)
+            .stream()
+            .map(ChatMessageCommonResponse::from)
+            .toList());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<List<ChatMessageCommonResponse>> getUsersMessageHistoryForAdmin(
+        @RequestParam Long userId, @PathVariable String id
+    ) {
+
+        return ResponseEntity.ok(chatService.getMessageHistory(userId, UUID.fromString(id))
+            .stream()
+            .map(ChatMessageCommonResponse::from)
+            .toList());
+    }
+
+    public ResponseEntity<List<ChatRoomAdminResponse>> getActiveChatRoomsForAdmin(
+        @RequestParam Long userId
+    ) {
+
+        return ResponseEntity.ok(chatService.getAllActiveRoomList(userId)
+            .stream()
+            .map(ChatRoomAdminResponse::from)
+            .toList());
+    }
+
+
+}
