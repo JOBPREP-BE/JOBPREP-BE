5eaa1b96c15956ad7b12576c80b01471a086ef96
oriduckduck <seocd777@gmail.com>
2024-12-15 00:56:06 +0900

    [FIX] GlobalExceptionHandler에서 다루는 JWT Exception이 jjwt 패키지가 아닌 auth0 패키지의 jwt인 것을 확인하고 후자 기반 JWT로직으로 수정.

--- src/main/java/io/dev/jobprep/security/jwt/JwtService.java
+++ src/main/java/io/dev/jobprep/security/jwt/JwtService.java
@@ -1,21 +1,21 @@
 package io.dev.jobprep.security.jwt;
 
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
 import io.dev.jobprep.security.jwt.dto.TokenInfo;
-import io.dev.jobprep.security.oauth.PrincipalDetails;
-import io.jsonwebtoken.*;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
+import com.auth0.jwt.JWT;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import java.security.Key;
+
+import java.time.Instant;
 import java.util.Date;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -29,82 +29,92 @@ public class JwtService {
     private Long refreshTokenValidityTime;
     //토큰 생성기
     //리프레쉬토큰, 액세스 토큰 재생성
+
+    private Algorithm getAlgorithm() {
+        return Algorithm.HMAC256(secretKey);
+    }
+
     public TokenInfo generateTokenInfo(String userId, String userEmail, String userRoles ){
+        // Create Access Token
+        String accessToken = generateAccessToken(userId, userEmail, userRoles);
+
+        // Create Refresh Token
+        String refreshToken = generateRefreshToken(userId);
 
         //JWT 토큰 반환
-        String accessToken = Jwts.builder()
-                .claim("auth" , userRoles )
-                .setSubject(userId)
-                .claim("email", userEmail)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis()+ accessTokenValidityTime))
-                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
-                .compact();
-
-        String refreshToken = Jwts.builder()
-                .setSubject(userId)
-                .setExpiration(new Date(System.currentTimeMillis()+ refreshTokenValidityTime))
-                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
-                .compact();
-
         return TokenInfo.builder()
-                .grantType("Bearer") // 토큰 타입 설정
-                .accessToken(accessToken) // 액세스 토큰 설정
-                .refreshToken(refreshToken) // 리프레시 토큰 설정
+                .grantType("Bearer")
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
                 .build();
 
     }
 
-    public Boolean isTokenValid(String token, UserDetails userDetails){
-        final String username = extractUsername(token);
-        try {
-            return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
-        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
-            log.info("Invalid JWT Token", e);
-        } catch (ExpiredJwtException e) {
-            log.info("Expired JWT Token", e);
-        } catch (UnsupportedJwtException e) {
-            log.info("Unsupported JWT Token", e);
-        } catch (IllegalArgumentException e) {
-            log.info("JWT claims string is empty.", e);
-        }
-        return false;
+    private String generateAccessToken(String userId, String userEmail, String userRoles ) {
+        return JWT.create()
+                .withSubject(userId)
+                .withClaim("auth", userRoles)
+                .withClaim("email", userEmail)
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + accessTokenValidityTime))
+                .sign(getAlgorithm());
+    }
+
+    private String generateRefreshToken(String userId) {
+        return JWT.create()
+                .withSubject(userId)
+                .withExpiresAt(new Date(System.currentTimeMillis() + refreshTokenValidityTime))
+                .sign(getAlgorithm());
+    }
+
+    public Boolean isTokenValid(String token) {
+        DecodedJWT decodedJWT = verifyToken(token);
+        return !isTokenExpired(decodedJWT);
     }
 
     //토큰 만료 확인
-    public boolean isTokenExpired(String token) {
-        return extractExpiration(token).before(new Date());
-    }
-    //토큰에서 추출한 claim에서 만료 날짜 추출
-    private Date extractExpiration(String token) {
-        return extractClaim(token , Claims::getExpiration);
-    }
-
-    //토큰 추출한 claim에서 Username(이메일을 뽑아옴)
-    public String extractUsername(String token){
-        return extractClaim(token, Claims::getSubject);
+    private boolean isTokenExpired(DecodedJWT decodedJWT) {
+        boolean expired = decodedJWT.getExpiresAt().before(new Date());
+        if(expired) {
+            Instant expirationTime = decodedJWT.getExpiresAt().toInstant();
+            throw new TokenExpiredException("Token expired on ", expirationTime);
+        }
+        return expired;
     }
 
-    //토큰에서 추출한 claim에서 원하는 claim을 가져옴
-    public <T> T extractClaim(String token, Function<Claims,T> claimsResolver) {
-        final Claims claims = extractAllClaims(token);
-        return claimsResolver.apply(claims);
+    // Extract username (subject) from token
+    public String extractUserId(String token) {
+        try {
+            return verifyToken(token).getSubject();
+        } catch (JWTDecodeException e) {
+            log.info("Error decoding JWT: {}", e.getMessage());
+            return null;
+        }
     }
 
-    //string 토큰에서 모든 claim 추출하여 Claim객체에 저장
-    private Claims extractAllClaims(String token){
-        return Jwts
-                .parserBuilder()
-                .setSigningKey(getSignInKey())//sign in key로 JWT parse
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
+    //Exract UserRole from token
+    public String extractUserEmail(String token) {
+        try {
+            return verifyToken(token).getClaim("email").asString();
+        } catch (JWTDecodeException e) {
+            log.info("Error decoding JWT: {}", e.getMessage());
+            return null;
+        }
     }
 
-    //signInKey를 hmac Sah 알고리즘을 이용해 암호키로 만든다
-    private Key getSignInKey(){
-        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
-        return Keys.hmacShaKeyFor(keyBytes);
+    public String extractUserRole (String token) {
+        try {
+            return verifyToken(token).getClaim("auth").toString();
+        } catch (JWTDecodeException e) {
+            log.info("Error decoding JWT: {}", e.getMessage());
+            return null;
+        }
+    }
+
+    // Verify and decode a JWT
+    private DecodedJWT verifyToken(String token) {
+        JWTVerifier verifier = JWT.require(getAlgorithm()).build();
+        return verifier.verify(token);
     }
 
 }
