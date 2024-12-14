aa8827299426d1fd139401ec8e9137236e0bbe86
oriduckduck <seocd777@gmail.com>
2024-12-14 17:50:44 +0900

    [FEAT] Oauth 로그인 성공시 refresh token과 access token을 헤더에 http only, Secure 설정을 걸어서 전달해주고, 지원현황 조회 페이지로 리다이렉팅

--- src/main/java/io/dev/jobprep/security/oauth/OAuth2SuccessHandler.java
+++ src/main/java/io/dev/jobprep/security/oauth/OAuth2SuccessHandler.java
@@ -1,24 +1,53 @@
 package io.dev.jobprep.security.oauth;
 
+import io.dev.jobprep.security.jwt.JwtService;
+import io.dev.jobprep.security.jwt.dto.TokenInfo;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Slf4j
 @Component
 public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
+    final JwtService jwtService;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+        String userId = principalDetails.getUserId().toString();
+        String userEmail = principalDetails.getUsername();
+        String userAuthority = principalDetails
+                .getAuthorities()
+                .stream().map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
 
+        TokenInfo tokenInfo = jwtService.generateTokenInfo(userId, userEmail, userAuthority);
 
+        // 쿠키 설정 - HttpOnly, Secure 설정 권장 (HTTPS 환경 필수)
+        Cookie accessTokenCookie = new Cookie("accessToken", tokenInfo.getAccessToken());
+        accessTokenCookie.setHttpOnly(true);
+        accessTokenCookie.setSecure(true);
+        accessTokenCookie.setPath("/");
+
+        Cookie refreshTokenCookie = new Cookie("refreshToken", tokenInfo.getRefreshToken());
+        refreshTokenCookie.setHttpOnly(true);
+        refreshTokenCookie.setSecure(true);
+        refreshTokenCookie.setPath("/");
+
+        response.addCookie(accessTokenCookie);
+        response.addCookie(refreshTokenCookie);
+
+        response.sendRedirect("/api/v1/applicationstatus/?userId="+userId);
     }
 }
