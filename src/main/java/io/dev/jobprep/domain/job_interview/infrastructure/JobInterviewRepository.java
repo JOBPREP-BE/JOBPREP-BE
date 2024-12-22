ecc63d928faa60b0a65087eafd6e80087697c2aa
Go-EunLee <koa789456@gmail.com>
2024-12-22 19:37:57 +0900

    [FEAT] Job Interview Repository Pagination 적용

--- src/main/java/io/dev/jobprep/domain/job_interview/infrastructure/JobInterviewRepository.java
+++ src/main/java/io/dev/jobprep/domain/job_interview/infrastructure/JobInterviewRepository.java
@@ -1,13 +1,17 @@
 package io.dev.jobprep.domain.job_interview.infrastructure;
 
 import io.dev.jobprep.domain.job_interview.domain.JobInterview;
+import io.dev.jobprep.domain.job_interview.presentation.dto.res.FindJobInterviewResponse;
 import io.dev.jobprep.domain.users.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface JobInterviewRepository extends JpaRepository<JobInterview, Long> {
-    List<JobInterview> findAllByCreator(User user);
+public interface JobInterviewRepository extends JpaRepository<JobInterview, Long> ,
+    JobInterviewRepositoryCustom {
+
 }
+
