7bee712defc9bc347456059adee552eb19b49eb8
JIN-076 <andy230@naver.com>
2024-12-24 00:10:56 +0900

    [FEAT] API Path 네이밍을 케밥 케이스로 수정

--- src/main/java/io/dev/jobprep/domain/applicationstatus/presentation/ApplicationStatusController.java
+++ src/main/java/io/dev/jobprep/domain/applicationstatus/presentation/ApplicationStatusController.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/applicationstatus")
+@RequestMapping("/api/v1/application-status")
 @RequiredArgsConstructor
 public class ApplicationStatusController implements ApplicationStatusSwagger {
 
