f6d35fceac9fc2f6d45ea9f32e4e59c00826ae2d
JIN-076 <andy230@naver.com>
2024-12-24 01:12:32 +0900

    [FEAT] 불필요한 오버라이딩 메서드 제거

--- src/main/java/io/dev/jobprep/system/internal/interceptor/IpWhiteListInterceptor.java
+++ src/main/java/io/dev/jobprep/system/internal/interceptor/IpWhiteListInterceptor.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Component
@@ -36,17 +35,5 @@ public class IpWhiteListInterceptor implements HandlerInterceptor {
             return false;
         }
         return true;
-    }
-
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-        ModelAndView modelAndView) throws Exception {
-        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
-    }
-
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
-        Object handler, Exception ex) throws Exception {
-        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
     }
 }
