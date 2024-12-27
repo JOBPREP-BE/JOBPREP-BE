aea12d07fae155f126d4172ab3354647516f7a8f
JIN-076 <andy230@naver.com>
2024-12-27 22:29:30 +0900

    [FEAT] Stomp 헤더의 heart-beat를 활용한 모니터링 이벤트 핸들러 구현

---
+++ src/main/java/io/dev/jobprep/common/event/WebSocketMonitoringEventListener.java
@@ -0,0 +1,38 @@
+package io.dev.jobprep.common.event;
+
+import io.dev.jobprep.common.stomp.StompTokenProvider;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class WebSocketMonitoringEventListener {
+
+    private final StompTokenProvider stompTokenProvider;
+
+    @EventListener
+    public void handleSessionConnected(SessionConnectedEvent event) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
+        String sessionId = accessor.getSessionId();
+        log.info("Connected: Session Id {}", sessionId);
+
+        // TODO: Redis 또는 DB에 세션 정보 저장
+        stompTokenProvider.verifyHeader(accessor);
+    }
+
+    @EventListener
+    public void handleSessionDisconnected(SessionDisconnectEvent event) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
+        String sessionId = accessor.getSessionId();
+        log.info("Disconnected: Session Id {}", sessionId);
+
+        stompTokenProvider.disconnectToChatRoom(sessionId);
+    }
+
+}
