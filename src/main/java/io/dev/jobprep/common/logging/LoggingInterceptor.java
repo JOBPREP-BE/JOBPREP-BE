7ac7a532bb3ee83af1e04b320846d12e01227881
JIN-076 <andy230@naver.com>
2025-01-01 22:26:17 +0900

    [SYNC] (refact) set logger interceptor, aspect with custom annotation

---
+++ src/main/java/io/dev/jobprep/common/logging/LoggingInterceptor.java
@@ -0,0 +1,45 @@
+package io.dev.jobprep.common.logging;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.WebRequestInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.WebRequestHandlerInterceptorAdapter;
+
+@Slf4j
+@Component
+public class LoggingInterceptor extends WebRequestHandlerInterceptorAdapter {
+
+    /**
+     * Create a new WebRequestHandlerInterceptorAdapter for the given WebRequestInterceptor.
+     *
+     * @param requestInterceptor the WebRequestInterceptor to wrap
+     */
+
+    public LoggingInterceptor(WebRequestInterceptor requestInterceptor) {
+        super(requestInterceptor);
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+        Object handler) throws Exception {
+        log.info("url : {}", request.getRequestURI());
+        return super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+        ModelAndView modelAndView) throws Exception {
+        log.info("response status: {}", response.getStatus());
+        super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
+        Object handler, Exception ex) throws Exception {
+        log.info("request Complete!");
+        super.afterCompletion(request, response, handler, ex);
+    }
+}
