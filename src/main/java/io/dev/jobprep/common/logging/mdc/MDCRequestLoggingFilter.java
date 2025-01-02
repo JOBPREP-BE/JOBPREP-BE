b6fb36eed1d094164e00c665dd38cccd80e88342
JIN-076 <andy230@naver.com>
2025-01-02 17:58:18 +0900

    [FEAT] request별 흐름 파악 및 로그 추적을 위한 MDC 설정

---
+++ src/main/java/io/dev/jobprep/common/logging/mdc/MDCRequestLoggingFilter.java
@@ -0,0 +1,37 @@
+package io.dev.jobprep.common.logging.mdc;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.UUID;
+import org.slf4j.MDC;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class MDCRequestLoggingFilter implements Filter {
+
+    private static final String REQUEST_HEADER = "X-RequestID";
+    private static final String ID = "request_id";
+    private static final String DASH = "-";
+    private static final String EMPTY = "";
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+        throws IOException, ServletException {
+
+        var requestId = ((HttpServletRequest) request).getHeader(REQUEST_HEADER);
+        MDC.put(ID, requestId == null || requestId.isBlank() ?
+            UUID.randomUUID().toString().replace(DASH, EMPTY) : requestId);
+
+        chain.doFilter(request, response);
+        MDC.clear();
+
+    }
+}
