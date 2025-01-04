948fb444786a81f62c3abf4cc986d5f7901928e2
서창덕 <44630705+Garodden@users.noreply.github.com>
2025-01-04 19:13:09 +0900

    [FEAT] ServerConfig HTTPS 포트 설정 8080 포트로 들어오는 HTTP 포트를 제외하고는 HTTPS로 리다이렉트

---
+++ src/main/java/io/dev/jobprep/core/configuration/ServerConfig.java
@@ -0,0 +1,30 @@
+package io.dev.jobprep.core.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServerConfig {
+
+    @Value("${server.port}")
+    private int httpsPort;
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(httpsPort);
+        return connector;
+    }
+}
