7d7b1f2d497b92525ebd261186bd7b16e781d958
서창덕 <44630705+Garodden@users.noreply.github.com>
2025-01-05 02:18:32 +0900

    [Feat] AccessDeniedHandler 에 log 추가

--- src/main/java/io/dev/jobprep/exception/CustomAccessDeniedHandler.java
+++ src/main/java/io/dev/jobprep/exception/CustomAccessDeniedHandler.java
@@ -5,6 +5,7 @@ import io.dev.jobprep.exception.dto.ErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -16,12 +17,17 @@ import static io.dev.jobprep.exception.code.ErrorCode403.AUTH_ACCESS_DENIED;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
     private final ObjectMapper objectMapper;  // 추가 필요
     @Override
     public void handle(HttpServletRequest request,
                        HttpServletResponse response,
                        AccessDeniedException accessDeniedException) throws IOException {
+
+        log.info("Access Denied Exception: {}, Path: {}",
+                accessDeniedException.getMessage(),
+                request.getRequestURI());
 
         response.setContentType("application/json");
         response.setStatus(HttpServletResponse.SC_FORBIDDEN); // 403
