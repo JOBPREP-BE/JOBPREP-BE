ee8f68591716a64531f1d54df468f23726e1e03f
서창덕 <44630705+Garodden@users.noreply.github.com>
2025-01-05 01:53:12 +0900

    [ReFactor] cors 설정 SecurityConfig에서 해결하도록 변경

--- src/main/java/io/dev/jobprep/core/configuration/SecurityConfig.java
+++ src/main/java/io/dev/jobprep/core/configuration/SecurityConfig.java
@@ -1,10 +1,8 @@
 package io.dev.jobprep.core.configuration;
 
 import io.dev.jobprep.domain.security.jwt.filter.JwtAuthenticationFilter;
-import io.dev.jobprep.domain.security.jwt.application.JwtService;
 import io.dev.jobprep.domain.security.oauth.application.CustomOAuth2UserService;
 import io.dev.jobprep.domain.security.oauth.application.OAuth2SuccessHandler;
-import io.dev.jobprep.domain.security.oauth.application.PrincipalDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +14,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @RequiredArgsConstructor
 @Configuration
@@ -25,8 +28,6 @@ public class SecurityConfig {
 
     private final CustomOAuth2UserService oAuth2UserService;
     private final OAuth2SuccessHandler oAuth2SuccessHandler;
-    private final JwtService jwtService;
-    private final PrincipalDetailsService principalDetailsService;
     private final AuthenticationEntryPoint entryPoint;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     @Bean
@@ -58,7 +59,6 @@ public class SecurityConfig {
             .sessionManagement(session -> session
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)  // JWT를 사용하므로 세션은 불필요
             );
-            ;
 
         return http.build();
     }
@@ -69,6 +69,25 @@ public class SecurityConfig {
             .csrf(AbstractHttpConfigurer::disable)
             .formLogin(AbstractHttpConfigurer::disable)
             .logout(AbstractHttpConfigurer::disable)
-            .cors(AbstractHttpConfigurer::disable);
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()));
+    }
+
+    //cors 설정
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://3.36.225.188",
+                "http://localhost:3000",
+                "http://127.0.0.1:3000",
+                "https://fe.jobprep.site"
+        ));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
