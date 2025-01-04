5b3e64abea4e09f8376077aa5a17973763260563
JIN-076 <andy230@naver.com>
2025-01-05 01:46:29 +0900

    [REFACT] Deferred Join + Covering Index를 활용한 오프셋 기반 페이지네이션 성능 최적화 (데이터 10,000,000건 기준으로 5.6배 향상)

--- src/main/java/io/dev/jobprep/domain/study/infrastructure/StudyJpaRepository.java
+++ src/main/java/io/dev/jobprep/domain/study/infrastructure/StudyJpaRepository.java
@@ -27,6 +27,18 @@ public interface StudyJpaRepository extends JpaRepository<Study, Long>, StudyRep
     Optional<Study> findGatheredStudyByUserId(Long userId);
 
     @Query(value = """
+        SELECT * FROM study AS std
+            JOIN (
+                    SELECT id FROM study
+                    WHERE study_status = 'RECRUITING' AND deleted_at IS NULL
+                    ORDER BY id DESC
+                    LIMIT :offset, :limit
+            ) AS temp USING (id)
+        ORDER BY std.id DESC
+    """, nativeQuery = true)
+    List<Study> findRecruitingStudyWithPagination(int offset, int limit);
+
+    @Query(value = """
         select * from study as s left join user_study us on s.id = us.study_id
         where s.study_status = 'RECRUITING' group by s.id having count(us.user_id) < :headCount
     """, nativeQuery = true)
--- src/main/java/io/dev/jobprep/domain/study/infrastructure/StudyRepositoryCustom.java
+++ src/main/java/io/dev/jobprep/domain/study/infrastructure/StudyRepositoryCustom.java
@@ -8,8 +8,5 @@ import java.util.Optional;
 public interface StudyRepositoryCustom {
 
     Optional<StudyWithStartDateDto> getStudyWithStartDate(Long studyId);
-
     List<Study> findNonDeletedStudyWithPagination(Long cursorId, int pageSize);
-
-    List<Study> findRecruitingStudyWithPagination(int page, int pageSize, int pageGroupSize);
 }
