93c091e5e86aefe66816a92b66c2cd309192a085
oriduckduck <seocd777@gmail.com>
2025-01-05 18:44:49 +0900

    [FEAT] HTTP 포트 80, 8080 열리도록 수정

--- src/main/java/io/dev/jobprep/core/configuration/ServerConfig.java
+++ src/main/java/io/dev/jobprep/core/configuration/ServerConfig.java
@@ -16,13 +16,14 @@ public class ServerConfig {
     @Bean
     public ServletWebServerFactory servletContainer() {
         TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        tomcat.addAdditionalTomcatConnectors(createHttpConnector(80));
+        tomcat.addAdditionalTomcatConnectors(createHttpConnector(8080));
         return tomcat;
     }
 
-    private Connector createStandardConnector() {
+    private Connector createHttpConnector(int port) {
         Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-        connector.setPort(8080);
+        connector.setPort(port);
         connector.setSecure(false);
         connector.setRedirectPort(httpsPort);
         return connector;
