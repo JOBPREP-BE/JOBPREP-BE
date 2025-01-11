60ff31acac86a965b8a9750eea96dfc0eea78490
OriDuckDuck <seocd777@gmail.com>
2025-01-12 06:13:56 +0900

    [FIX] 스웨거 수정

--- src/main/java/io/dev/jobprep/common/swagger/template/OauthSwagger.java
+++ src/main/java/io/dev/jobprep/common/swagger/template/OauthSwagger.java
@@ -7,6 +7,7 @@ import io.dev.jobprep.domain.security.oauth.presentation.dto.TokenResponse;
 import io.dev.jobprep.exception.dto.ErrorResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.headers.Header;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -16,24 +17,19 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 @Tag(name = "OAuth", description = "OAuth로그인용 path 조회")
 public interface OauthSwagger {
-
     @Operation(summary = "토큰 재발급", description = "Refresh 토큰을 사용하여 새로운 Access 토큰을 발급받는 API")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "토큰 재발급 성공",
-                    content = @Content(
-                            mediaType = "application/json",
-                            schema = @Schema(implementation = TokenInfo.class),
-                            examples = @ExampleObject(value =  """
-                            {
-                              "grantType": "Bearer",
-                              "authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyIiwiYXV0aCI6Ik5PUk1BTCIsImVtYWlsIjoic2VvY2QxMjM0NTZAbmF2ZXIuY29tIiwiaWF0IjoxNzM0MTkxNDk4LCJleHAiOjE3MzQyOTk0OTh9.PYvvHL3Ito6r-ls_8Rn1s4exhEt1TEPoBGzpXoF0ENA",
-                              "xRefreshToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyIiwiZXhwIjoxNzM2NzgzNDk4fQ.EcWwUnuJ2_Csfb5MEX1isTI_v3P5SCLKvkUt_-zbLtg"
-                            }
-                            """))),
+                    content = @Content(mediaType = "application/json"),
+                    headers = {
+                        @Header(name = "Set-Cookie-At", description = "Access 토큰 쿠키 설정 헤더",schema = @Schema(type = "string", example = "Authorization=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyIiwiYXV0aCI6Ik5PUk1BTCIsImVtYWlsIjoic2VvY2Q...")),
+                        @Header(name = "Set-Cookie-Rt", description = "Refresh 토큰 쿠키 설정 헤더", schema = @Schema(type = "string", example = "XRefreshTokens=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyIiwiZXhwIjoxNzM2NzgzNDk4fQ.EcWwUnuJ2_Csfb5..."))
+                    }),
             @ApiResponse(responseCode = "401", description = "토큰이 만료되었을 떄",
                     content = @Content(
                             mediaType = "application/json",
@@ -61,11 +57,12 @@ public interface OauthSwagger {
                             examples = @ExampleObject(name = "E01-JWT-003", value = SwaggerJwtErrorExamples.RFT_VALIDATION_FAILURE))),
 
     })
-    ResponseEntity<TokenResponse> reissue(
+    ResponseEntity<Void> reissue(
             @Parameter(description = "갱신용 리프레시 토큰", required = true)
-            @RequestHeader(value = "XRefreshToken") String refreshToken,
+            @CookieValue(value = "XRefreshTokens") String refreshToken,
             @Parameter(description = "HTTP 응답 객체", required = true)
-                    HttpServletResponse response);
+            HttpServletResponse response);
+
 
     @Operation(summary = "로그아웃", description = "사용자 로그아웃을 처리하는 API")
     @ApiResponses(value = {
