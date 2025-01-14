4debfbcf40e214c6e8df8e834bbbea0b4d74825c
서창덕 <44630705+Garodden@users.noreply.github.com>
2025-01-15 00:57:51 +0900

    [FEAT] 임시토큰 에러 예시 추가

---
+++ src/main/java/io/dev/jobprep/core/properties/swagger/error/SwaggerTempTokenErrorExamples.java
@@ -0,0 +1,8 @@
+package io.dev.jobprep.core.properties.swagger.error;
+
+public class SwaggerTempTokenErrorExamples {
+
+    public static final String TMPT_CACHE_FALIURE = "{\"timestamp\":\"2024-12-02T10:07:31.404Z\",\"statusCode\":400,\"code\":\"E01-TMPT-001\",\"message\":\"임시 인증 토큰 캐싱을 실패했습니다.\"}";
+    public static final String TMPT_CACHE_DELETION_FALIURE= "{\"timestamp\":\"2024-12-02T10:07:31.404Z\",\"statusCode\":400,\"code\":\"E01-TMPT-002\",\"message\":\"임시 인증 토큰 캐시 삭제를 실패했습니다.\"}";
+    public static final String TMPT_VALIDATION_FAILURE = "{\"timestamp\":\"2024-12-02T10:07:31.404Z\",\"statusCode\":400,\"code\":\"E01-TMPT-003\",\"message\":\"임시 인증 토큰 캐시 검증을 실패했습니다.\"}";
+}
