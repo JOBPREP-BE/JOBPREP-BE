7613491d47966af4988074efd0d22aa798120903
서창덕 <44630705+Garodden@users.noreply.github.com>
2025-01-16 18:46:15 +0900

    [REFACTOR] 토큰 커스텀 예외 이름 수정

--- src/main/java/io/dev/jobprep/domain/security/jwt/exception/TokenStorageException.java
+++ src/main/java/io/dev/jobprep/domain/security/jwt/exception/TokenStorageException.java
@@ -3,8 +3,8 @@ package io.dev.jobprep.domain.security.jwt.exception;
 import io.dev.jobprep.exception.code.ErrorCode;
 import io.dev.jobprep.exception.exception_class.CustomException;
 
-public class TokenException extends CustomException {
-    public TokenException(ErrorCode errorCode) {
+public class TokenStorageException extends CustomException {
+    public TokenStorageException(ErrorCode errorCode) {
         super(errorCode);
     }
 }
