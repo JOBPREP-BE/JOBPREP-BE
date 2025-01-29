512d3a1e99dcfec806ac97bf3b5470f57220799c
JIN-076 <57834671+JIN-076@users.noreply.github.com>
2025-01-29 23:17:43 +0900

    [REFACT] health-check를 위한 간단한 내부 API 호출을 수행하도록 변경 (#76)

--- src/main/java/io/dev/jobprep/system/internal/health/HealthCheckController.java
+++ src/main/java/io/dev/jobprep/system/internal/health/HealthCheckController.java
@@ -4,12 +4,12 @@ import io.dev.jobprep.common.actuator.ApplicationHealthIndicator;
 import io.dev.jobprep.system.internal.developer.DeveloperTokenHelper;
 import io.dev.jobprep.util.IpAddressHelper;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
@@ -21,6 +21,11 @@ public class HealthCheckController {
 
     private final ApplicationHealthIndicator healthIndicator;
     private final DeveloperTokenHelper developerTokenHelper;
+
+    @GetMapping("/server")
+    public ResponseEntity<Void> serverHealthCheck(HttpServletRequest req, HttpServletResponse res) {
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
 
     @PutMapping("/up")
     public void up(HttpServletRequest request, @RequestHeader(AUTHORIZATION) String token) {
