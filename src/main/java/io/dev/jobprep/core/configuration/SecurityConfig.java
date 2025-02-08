619fd081fbcc21a5c3db41dedbbd50acb74f6732
홍지인 <57834671+JIN-076@users.noreply.github.com>
2025-02-08 21:41:28 +0900

    Bug/#257 OAuth2 인증 성공 후 redirect 호출 시, /login에 걸리는 문제 해결 ( + JwtFilter를 타지 않도록 변경 ) (#85)
    * [BUG] OAuth 인증 절차 성공 후 JWT 토큰 생성을 위한 리다이렉션 호출 시, /login으로 잘못 리다이렉션되는 문제 해결, /callback 엔드포인트에 permitAll 부여
    
    * [STYLE] 줄바꿈 추가
    
    * [STYLE] 불필요한 줄바꿈 제거
    
    * [BUG] /callback 엔드포인트가 JwtFilter에 걸리지 않도록 변경

--- src/main/java/io/dev/jobprep/core/configuration/SecurityConfig.java
+++ src/main/java/io/dev/jobprep/core/configuration/SecurityConfig.java
@@ -46,6 +46,7 @@ public class SecurityConfig {
                                 "/api/v1/oauth2/callback",
                                 "/login/oauth2/code/**",
                                 "/oauth2/authorization/**",
+                                "/oauth2/callback",
                                 "/api-docs/**",
                                 "/swagger-ui/**",
                                 "/actuator/**",
@@ -119,7 +120,8 @@ public class SecurityConfig {
         return web -> web.ignoring()
                 .requestMatchers(
                         "/actuator/**",
-                        "/api/v1/oauth2/reissue"
+                        "/api/v1/oauth2/reissue",
+                        "/oauth/callback"
                 );
     }
 }
