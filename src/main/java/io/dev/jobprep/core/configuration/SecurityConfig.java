e64766003dc31aeede2303cc4ed0a93ba4d7fbd4
JIN-076 <andy230@naver.com>
2025-02-10 16:21:30 +0900

    [BUG] redirectUri명 오류 수정

--- src/main/java/io/dev/jobprep/core/configuration/SecurityConfig.java
+++ src/main/java/io/dev/jobprep/core/configuration/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
                                 "/api/v1/oauth2/callback",
                                 "/login/oauth2/code/**",
                                 "/oauth2/authorization/**",
-                                "/oauth2/callback",
+                                "/oauth/callback",
                                 "/api-docs/**",
                                 "/swagger-ui/**",
                                 "/actuator/**",
